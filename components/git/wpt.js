25e9283e1e62cf0c5f882c94dce732a4c4ac7084
Joyee Cheung <joyeec9h3@gmail.com>
2018-06-16 10:45:28 +0800

    wpt: update repo location, add README generator

--- components/git/wpt.js
+++ components/git/wpt.js
@@ -7,28 +7,34 @@ const auth = require('../../lib/auth');
 const URLTestUpdater = require('../../lib/wpt/url');
 const { runPromise } = require('../../lib/run');
 
-// web-platform-tests/url/${src}: node/test/fixtures/web-platform-tests/url/${dest}
-const base = '.ncu/cache/wpt';
+// web-platform-tests/url/file: node/test/fixtures/wpt/url/file
 
 function builder(yargs) {
   return yargs
     .positional('name', {
       describe: 'Subset of the WPT to update, e.g. \'url\'',
       type: 'string'
+    })
+    .options({
+      dest: {
+        describe: 'Directory the assets will be put under',
+        type: 'string',
+        default: '.ncu/cache/wpt'
+      }
     });
 }
 
-async function url() {
+async function url(argv) {
   const cli = new CLI();
   const credentials = await auth();
   const request = new Request(credentials);
   const updater = new URLTestUpdater(cli, request);
-  await updater.update(base);
+  await updater.writeFiles(argv.dest);
 }
 
 function handler(argv) {
   if (argv.name === 'url') {
-    runPromise(url());
+    runPromise(url(argv));
   } else {
     yargs.showHelp();
   }
